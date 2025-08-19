384f705c5bf645d578e4d86deaead3303b0cdc01
DanielaFernandaOjedaArrelucea <daniela.oxeda@gmail.com>
2025-08-19 12:47:25 -0500

    feat(js): implementar lista de amigos con validación de entrada y renderizado en DOM
    - Se añadió un array para almacenar los amigos
    - Se implementó función para renderizar la lista dinámicamente en <ul>
    - Se agregó validación del input y limpieza del campo tras añadir

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigoSecreto = "";
+let lista = []; //array de amigos
+
+function agregarAmigo() {
+    // Captura el valor del input
+    const input = document.getElementById('amigo');
+    // Valida que el campo no esté vacío
+    if (input.value.trim() === "") {
+        alert('Por favor, inserte un nombre.')
+    } else {
+        // Guardar el nombre en el array
+        lista.push(input.value);
+        // Actualizar la lista visual en el HTML
+        asignarListaElementos();
+        // Limpiar campo
+        limpiarInput();
+    }
+}
+
+// Limpia el campo de entrada
+function limpiarInput() {
+    document.querySelector('#amigo').value = '';
+}
+
+function asignarListaElementos() {
+    // Selecciona el ul del HTML
+    let ulLista = document.querySelector('#listaAmigos');
+    // Limpia el contenido para no repetir
+    ulLista.innerHTML = "";
+    // Recorrer el array y se crea un <li> por cada amigo
+    for (let amigo of lista) {
+        // Se crea el <li>
+        let li = document.createElement("li");
+        //Asigna nombre
+        li.textContent = amigo;
+        // Lo mete al <ul>
+        ulLista.appendChild(li);
+    }
+}
+
